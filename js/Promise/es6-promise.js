53c82597b65803255ff30129fbdbc66f23f90051
g4rick <285699093@qq.com>
2019-08-17 15:31:41 +0800

    添加then方法

--- js/Promise/es6-promise.js
+++ js/Promise/es6-promise.js
@@ -4,21 +4,42 @@ const STATUS = {
   rejected: 'REJECTED'
 };
 
-const resolve = () => {
-  promise.status = STATUS.resolved;
-}
-
-
-const reject = () => {
-  promise.status = STATUS.rejected;
-}
+const statusProvider = (promise, status, data) => {
+  if (promise.status !== STATUS.pending) {
+    return false;
+  }
+  promise.result = data;
+  promise.status = status;
+};
 
 class MyPromise {
   constructor(executor) {
     this.status = STATUS.pending;
-    executor((data) => resolve(this, data), (data) => reject(this, data));
+    this.result = null;
+    executor(
+      data => statusProvider(this, STATUS.resolved, data),
+      data => statusProvider(this, STATUS.rejected, data)
+    );
+  }
+
+  then(...args) {
+    switch (this.status) {
+      case STATUS.pending: {
+        break;
+      }
+      case STATUS.resolved: {
+        args[0](this.result);
+        break;
+      }
+      case STATUS.rejected: {
+        args[1](this.result);
+        break;
+      }
+    }
   }
 }
 
-const promise = new MyPromise((resolve, reject) => {});
-console.log(promise.resolve());
+const promise = new MyPromise((resolve, reject) => {
+  resolve();
+});
+console.log(promise);
